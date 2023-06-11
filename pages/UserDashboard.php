2cd119e882106404e488c5363e6367ed87eb0edc
wajeeNPC <shaheedwajee@gmail.com>
2023-06-11 11:35:25 +0530

    servise css done

--- pages/UserDashboard.php
+++ pages/UserDashboard.php
@@ -18,6 +18,7 @@ if($role==1){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/Dashboard.css">
     <link rel="stylesheet" href="../styles/DashboardIncludes.css">
+    <link rel="stylesheet" href="../styles/serv.css">
    
     <title>Dashboard</title>
 </head>
@@ -43,7 +44,6 @@ if($role==1){
                         <h1>Services</h1>
                         <div class="whole">
                             <div class="wrapper">
-                                <img id="left" src="../images/angle-left-solid.svg">
                                 <ul class="service-div">
                                         <li class="card">
                                             <h2>Title</h2>
@@ -70,8 +70,11 @@ if($role==1){
                                             <p>Somthing</p>
                                     </li>
                                 </ul>
+                                <div class="arrow">
+                                <img id="left" src="../images/angle-left-solid.svg">
                             <img id="right" src="../images/angle-right-solid.svg">
                         </div>
+    </div>
                         </div>
                     </div>
 
